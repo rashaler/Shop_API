91f55b08c8f2685c490a80f2c5bc4466339ae234
rashaler <richard.shaler@gmail.com>
2018-01-01 18:09:26 +0000

    Add orders and update HTTP response code from 200 to 201 in products

---
+++ api/routes/orders.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+       message: 'Orders were fetched' 
+    });
+    
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+       message: 'Order was created' 
+    });
+    
+});
+
+router.get('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+    if(id === 'special') {
+        res.status(200).json({
+            message: 'You discoverd the special Id',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'Order details',
+            orderId: req.params.orderId
+        });
+    }
+
+});
+
+router.delete('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+    if(id === 'special') {
+        res.status(200).json({
+            message: 'Cannot delete special Id',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'Deleted r',
+            orderId: req.params.orderId
+        });
+    }
+
+});
+
+
+module.exports = router;
