4cc67086828b877926cddfb061596f44718b3164
rashaler <richard.shaler@gmail.com>
2018-01-01 17:19:44 +0000

    Add HTTP patch and delete operations

--- api/routes/products.js
+++ api/routes/products.js
@@ -30,4 +30,35 @@ router.get('/:productId', (req, res, next) => {
 
 });
 
+router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if(id === 'special') {
+        res.status(200).json({
+            message: 'Updated special Id!',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'Updated product!'
+        });
+    }
+
+});
+
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if(id === 'special') {
+        res.status(200).json({
+            message: 'Cannot delete special Id!',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'Deleted product!'
+        });
+    }
+
+});
+
+
 module.exports = router;
